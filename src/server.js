95d7365099340aa3ec6ae1c1b6a1b4904be3632d
unknown <javier.rey.eu@gmail.com>
2026-06-18 15:41:48 +0100

    add xml content type to server

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ const contentTypes = {
   '.html': 'text/html; charset=utf-8',
   '.js': 'text/javascript; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
+  '.xml': 'text/xml; charset=utf-8',
   '.md': 'text/markdown; charset=utf-8',
   '.txt': 'text/plain; charset=utf-8',
   '.json': 'application/json',
